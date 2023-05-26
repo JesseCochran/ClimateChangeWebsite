eca6110e358d06ad54e0806f044f0c93ac7418bf
Jesse Cochran <s4007036@student.rmit.edu.au>
2023-05-26 20:26:53 +1000

    tried adding a bit of javascript to make drop down

--- src/main/java/app/PageST2C.java
+++ src/main/java/app/PageST2C.java
@@ -79,31 +79,51 @@ public class PageST2C implements Handler {
                         this is because oceans have a higher heat capacity compared to land therefore meaning they absorb and release heat slower which can then help show greater discrepancies in temperatures therefore showing signs of climate change. </p>
                         """;
 
+        // All the Drop down menu stuff for the data to eventually be retrieved from
         html = html + "<form action='/page2C.html' method='post'>";
 
         html = html + "   <div class='form-group'>";
         html = html + "      <label for='StartYear_drop'>Select the start year (Dropdown):</label>";
-        html = html + "      <select id='StartYear_drop' name='StartYear_drop'>";
+        html = html
+                + "      <select id='StartYear_drop' name='StartYear_drop' onchange='updateEndYearOptions(this.value)' size='1'>";
+        // This onchange section ^ makes the website more dynamic by using a java script
+        // function to take the value the user selects to then place it in the end date
+        // drop down menu.
+        // this connects the database to the start date drop down box.
         JDBCConnection jdbc = new JDBCConnection();
         ArrayList<Climate> years = jdbc.getLandOceanYears();
         for (Climate year : years) {
             html = html + "<option>" + year.getYear() + "</option>";
         }
-        html = html + "         <option>2010</option>";
-        html = html + "         <option>2011</option>";
-        html = html + "         <option>2012</option>";
         html = html + "      </select>";
         html = html + "   </div>";
+        // Using some javascript to change the value of the end year drop down section
+        // to be the same as the start year
+        html = html + "<script>";
+        html = html + "   function updateEndYearOptions(selectedItem) {";
+        html = html + "       var endYearDropdown = document.getElementById('EndYear_drop');";
+        html = html + "       endYearDropdown.innerHTML = '';"; // Clear existing options
 
+        // This javascript code effectively makes the data in the end year section must
+        // be in the range of the user selected date to the end of the avilable data in
+        // the database.
+        html = html + "       var selectedYear = parseInt(selectedItem);";
+        html = html + "       var EndYear = " + years.get(years.size() - 1).getYear() + ";";
+        html = html + "       for (var year = selectedYear; year <= EndYear; year++) {";
+        html = html + "           var option = document.createElement('option');";
+        html = html + "           option.text = year;";
+        html = html + "           option.value = year;";
+        html = html + "           endYearDropdown.add(option);";
+        html = html + "       }";
+        html = html + "   }";
+        html = html + "</script>";
+        // element of the end year drop down box
         html = html + "   <div class='form-group'>";
         html = html + "      <label for='EndYear_drop'>Select the end year (Dropdown):</label>";
-        html = html + "      <select id='EndYear_drop' name='EndYear_drop'>";
-        html = html + "         <option>2010</option>";
-        html = html + "         <option>2011</option>";
-        html = html + "         <option>2012</option>";
-        html = html + "         <option>2013</option>";
+        html = html + "      <select id='EndYear_drop' name='EndYear_drop' size='1'>";
         html = html + "      </select>";
         html = html + "   </div>";
+        // submit button
         html = html + "   <button type='submit' class='btn btn-primary'>Submit Year Range</button>";
 
         html = html + "</form>";
@@ -154,23 +174,6 @@ public class PageST2C implements Handler {
     public String outputData(String Year) {
         String html = "";
         html = html + "<h2>" + Year + " Climate Data</h2>";
-
-        // 🚨 This block of code is for Pre-Req students.
-        // Altneratively we can use JDBCConnection to add HTML for the movies list
-        // Uncomment the code to use the JDBCConnection Objects example(s)
-
-        // THIS IS FOR THE DATA BASE CONNECTION
-        // JDBCConnection jdbc = new JDBCConnection();
-        // ArrayList<LGA> Climates = jdbc.getDataByYearRange(Year);
-        // html = html + "<h2>" +
-        // Year +
-        // " Movies with Years (from JDBCConnection)</h2>" +
-        // "<ul>";
-        // for (LGA Climate : Climates) {
-        // html = html + "<li>" + Climate.name + " was made in " + Climate.year +
-        // "</li>";
-        // }
-        // html = html + "</ul>";
 
         return html;
     }
