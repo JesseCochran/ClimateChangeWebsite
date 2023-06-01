731428fa19239d6d309106fc504ebe739d59d138
Jesse Cochran <s4007036@student.rmit.edu.au>
2023-06-01 20:26:02 +1000

    satisfied with year selection in drop downs

--- src/main/java/app/PageST2C.java
+++ src/main/java/app/PageST2C.java
@@ -133,9 +133,9 @@ public class PageST2C implements Handler {
         html = html + "</script>";
 
         html = html + "   <div class='form-group'>";
-        html = html + "      <label for='StartYear_drop'>Select the start year (Dropdown):</label>";
+        html = html + "      <label for='StartYear_drop'>Select the start year:</label>";
         html = html
-                + "      <select id='StartYear_drop' name='StartYear_drop' onchange='updateEndYearOptions(this.value)' size='1'>";
+                + "      <select id='StartYear_drop' name='StartYear_drop' onchange='updateEndYearOptions()' size='1'>";
         // This onchange section ^ makes the website more dynamic by using a java script
         // Used java script taught in this video
         // https://www.youtube.com/watch?v=SBmSRK3feww&t=7s and my own knowledge
@@ -151,33 +151,76 @@ public class PageST2C implements Handler {
         html = html + "      </select>";
         html = html + "   </div>";
         // Using some javascript to change the value of the end year drop down section
-        // to be the same as the start year
+        // to be the same as the start year and vise versa
         html = html + "<script>";
-        html = html + "   function updateEndYearOptions(selectedItem) {";
-        html = html + "       var endYearDropdown = document.getElementById('EndYear_drop');";
-        html = html + "       endYearDropdown.innerHTML = '';"; // Clear existing options
+        html = html + "var selectedStartYear = null;";
+        html = html + "var selectedEndYear = null;";
+        html = html + "function updateEndYearOptions() {";
+        html = html + "   var startYearDropdown = document.getElementById('StartYear_drop');";
+        html = html + "   var EndYearDropdown = document.getElementById('EndYear_drop');";
+        html = html + "   var startYear = parseInt(startYearDropdown.value);";
+        html = html + "   var endYear = " + years.get(years.size() - 1).getYear() + ";";
+        html = html + "   var selectedEndYear = parseInt(EndYearDropdown.value);"; // Store selected value
+        html = html + "   EndYearDropdown.innerHTML = '';"; // Clear existing options
 
         // This javascript code effectively makes the data in the end year section must
         // be in the range of the user selected date to the end of the avilable data in
         // the database.
-        html = html + "       var selectedYear = parseInt(selectedItem);";
-        html = html + "       var EndYear = " + years.get(years.size() - 1).getYear() + ";";
-        html = html + "       for (var year = selectedYear; year <= EndYear; year++) {";
-        html = html + "           var option = document.createElement('option');";
-        html = html + "           option.text = year;";
-        html = html + "           option.value = year;";
-        html = html + "           endYearDropdown.add(option);";
-        html = html + "       }";
+        html = html + "   for (var year = startYear; year <= endYear; year++) {";
+        html = html + "       var option = document.createElement('option');";
+        html = html + "       option.text = year;";
+        html = html + "       option.value = year;";
+        html = html + "       EndYearDropdown.add(option);";
         html = html + "   }";
+        html = html + "   if (selectedEndYear) EndYearDropdown.value = selectedEndYear;"; // Reapply selected value
+        html = html + "}";
+        // this code does the same for the start year value
+        html = html + "function updateStartYearOptions() {";
+        html = html + "   var startYearDropdown = document.getElementById('StartYear_drop');";
+        html = html + "   var EndYearDropdown = document.getElementById('EndYear_drop');";
+        html = html + "   var EndYear = parseInt(EndYearDropdown.value);";
+        html = html + "   var selectedStartYear = parseInt(startYearDropdown.value);"; // Store selected value
+        html = html + "   startYearDropdown.innerHTML = '';"; // Clear existing options
+        html = html + "   for (var year = " + years.get(0).getYear() + "; year <= EndYear; year++) {";
+        html = html + "       var option = document.createElement('option');";
+        html = html + "       option.text = year;";
+        html = html + "       option.value = year;";
+        html = html + "       startYearDropdown.add(option);";
+        html = html + "   }";
+        html = html + "   if (selectedStartYear) startYearDropdown.value = selectedStartYear;"; // Reapply selected
+                                                                                                // value
+        html = html + "}";
         html = html + "</script>";
 
-        // element of the end year drop down box
         html = html + "   <div class='form-group'>";
-        html = html + "<p></p>";
-        html = html + "      <label for='EndYear_drop'>Select the end year (Dropdown):</label>";
-        html = html + "      <select id='EndYear_drop' name='EndYear_drop' size='1'>";
+        html = html + "      <label for='EndYear_drop'>Select the end year:</label>";
+        html = html
+                + "      <select id='EndYear_drop' name='EndYear_drop' onchange='updateStartYearOptions()' size='1'>";
+
+        // this connects the database to the start date drop down box.
+        // String StartYear_drop = context.formParam("StartYear_drop");
+        // jdbc = new JDBCConnection();
+        // years = jdbc.getLandOceanYears();
+        // for (int i = 0; i < years.size(); i++) {
+        // if (StartYear_drop != null && !StartYear_drop.isEmpty()
+        // && years.get(i).getYear() >= Integer.parseInt(StartYear_drop)) {
+        // html = html + "<option>" + years.get(i).getYear() + "</option>";
+        // }
+        // }
+        for (Climate year : years) {
+            html = html + "<option>" + year.getYear() + "</option>";
+        }
         html = html + "      </select>";
         html = html + "   </div>";
+
+        // element of the end year drop down box
+        // html = html + " <div class='form-group'>";
+        // html = html + "<p></p>";
+        // html = html + " <label for='EndYear_drop'>Select the end year
+        // (Dropdown):</label>";
+        // html = html + " <select id='EndYear_drop' name='EndYear_drop' size='1'>";
+        // html = html + " </select>";
+        // html = html + " </div>";
 
         // Sorting order
         html = html + """
