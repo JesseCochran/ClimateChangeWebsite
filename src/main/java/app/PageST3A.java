0542a1aa22d14b5320fd245db138936eccfe6066
Hudson Ciavarella <s3997086@student.rmit.edu.au>
2023-06-17 15:45:03 +1000

    Created html form 3A

--- src/main/java/app/PageST3A.java
+++ src/main/java/app/PageST3A.java
@@ -137,15 +137,17 @@ public class PageST3A implements Handler {
 
     // Add HTML for the page content
     html = html + """
-        <h2>Identify changed in temperature over extended periods</h2>
+        <h2>Identify changes in temperature over extended periods</h2>
         """;
 
     html = html + "<form action='/page2A.html' method='post'>";
 
     // geo location drop down
     html = html + "<div class='form-group'>";
+    html = html + "<h4>Choose a Geographic Type & Location</h4>";
     html = html + "     <label for='GeoLocation_drop'>Select Global, Country, State or City:</label>";
     html = html + "     <select id='GeoLocation_drop' name='GeoLocation_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select type--</option>";
     html = html + "     <option>Global</option>";
     html = html + "     <option>Country</option>";
     html = html + "     <option>State</option>";
@@ -156,33 +158,151 @@ public class PageST3A implements Handler {
     ArrayList<Climate> countryNames = jdbc.getCountryName();
 
     // country select dropdown
-    html = html + "<p>";
-    html = html + "     <label for='Country_drop'>Select Country:</label>";
-    html = html + "     <select id='Country_drop' name='Country_drop' size='1'>";
+    html = html + "     <label for='Country1_drop'>Select a Country:</label>";
+    html = html + "     <select id='Country1_drop' name='Country1_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select country--</option>";
     for (Climate countryName : countryNames) {
       html = html + "<option>" + countryName.getCountryName() + "</option>";
     }
-    html = html + "     </select>";
-    html = html + "</p>";
+    html = html + "</select>";
 
+    ArrayList<Climate> stateNames = jdbc.getStateName();
     // state select dropdown
-    html = html + "<p class='hide-element'>";
-    html = html + "     <label for='State_drop'>Select State:</label>";
-    html = html + "     <select id='State_drop' name='State_drop' size='1'>";
-    // insert loop that enters StateName's
-    html = html + "     </select>";
-    html = html + "</p>";
-
+    html = html + "     <label for='State1_drop'>Select a State:</label>";
+    html = html + "     <select id='State1_drop' name='State1_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select state--</option>";
+    for (Climate stateName : stateNames) {
+      html = html + "<option>" + stateName.getStateName() + "</option>";
+    }
+    html = html + "</select>";
+    
+    ArrayList<Climate> cityNames = jdbc.getCityName();
     // city select dropdown
-    html = html + "<p class='hide-element'>";
-    html = html + "     <label for='Cty_drop'>Select State:</label>";
-    html = html + "     <select id='City_drop' name='City_drop' size='1'>";
-    // insert loop that enters CityName's
+    html = html + "     <label for='City1_drop'>Select a City:</label>";
+    html = html + "     <select id='City1_drop' name='City1_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select city--</option>";
+    for (Climate cityName : cityNames) {
+      html = html + "<option>" + cityName.getCityName() + "</option>";
+    }
     html = html + "     </select>";
-    html = html + "</p>";
     html = html + "</div>";
 
+    html = html + "<div class='form-group'>";
+    ArrayList<Climate> years = jdbc.getYearRange3A();
+    html = html + "<h4>Choose a Start Year & Time Period</h4>";
+    html = html + "<label for='StartYear1_drop'>Select a Start Year:</label>";
+    html = html + "<select id='StarYear1_drop name='StartYear2_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select year--</option>";
+    for (Climate year : years) {
+      html = html + "<option>" + year.getYear() + "</option>";
+    }
+    html = html + "</select>";
+
+    html = html + "<label for='TimePeriod_drop'>Select the Time Period:</label>";
+    html = html + "<select id='TimePeriod_drop name='TimePeriod_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select period--</option>";
+    html = html + "<option>1 year</option>";
+    for (int i = 2; i < years.size() + 1; ++i) {
+      html = html + "<option>" + i + " years</option>";
+    }
+    html = html + "</select>";
+    html = html + "</div>";
+
+    html = html + "<div class='form-group'>";
+    html = html + "<h4>Add More Start Years</h4>";
+    html = html + "<label for='StartYear2_drop'>Add a Start Year:</label>";
+    html = html + "<select id='StarYear2_drop name='StartYear2_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select year--</option>";
+    for (Climate year : years) {
+      html = html + "<option>" + year.getYear() + "</option>";
+    }
+    html = html + "</select>";
+
+    html = html + "<label for='StartYear3_drop'>Add a Start Year:</label>";
+    html = html + "<select id='StarYear3_drop name='StartYear3_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select year--</option>";
+    for (Climate year : years) {
+      html = html + "<option>" + year.getYear() + "</option>";
+    }
+    html = html + "</select>";
+
+    html = html + "<label for='StartYear4_drop'>Add a Start Year:</label>";
+    html = html + "<select id='StarYear4_drop name='StartYear4_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select year--</option>";
+    for (Climate year : years) {
+      html = html + "<option>" + year.getYear() + "</option>";
+    }
+    html = html + "</select>";
+
+    html = html + "<label for='StartYear5_drop'>Add a Start Year:</label>";
+    html = html + "<select id='StarYear5_drop name='StartYear5_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select year--</option>";
+    for (Climate year : years) {
+      html = html + "<option>" + year.getYear() + "</option>";
+    }
+    html = html + "</select>";
+    html = html + "</div>";
+
+    html = html + "<div class='form-group'>";
+    html = html + "<h4>Add More Locations to Compare</h4>";
+    html = html + "     <label for='Country2_drop'>Add a Country:</label>";
+    html = html + "     <select id='Country2_drop' name='Country2_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select country--</option>";
+    for (Climate countryName : countryNames) {
+      html = html + "<option>" + countryName.getCountryName() + "</option>";
+    }
+    html = html + "</select>";
+
+    html = html + "     <label for='Country3_drop'>Add a Country:</label>";
+    html = html + "     <select id='Country3_drop' name='Country3_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select country--</option>";
+    for (Climate countryName : countryNames) {
+      html = html + "<option>" + countryName.getCountryName() + "</option>";
+    }
+    html = html + "</select>";
+
+    html = html + "     <label for='Country4_drop'>Add a Country:</label>";
+    html = html + "     <select id='Country4_drop' name='Country4_drop' size='1'>";
+    html = html + "<option value='' disabled selected hidden>--select country--</option>";
+    for (Climate countryName : countryNames) {
+      html = html + "<option>" + countryName.getCountryName() + "</option>";
+    }
+    html = html + "</select>";
+    html = html + "</div>";
+
+    // Sorting order
+    html = html + """
+      <div class='form-group'>
+      <h4>Sort By </h4>
+      <input type='radio' id ='SortOrder' name = 'SortOrder' value='False' checked>
+      <label class='radio-label' for='False'>Neither</label>
+      <input type='radio' id='SortOrder' name='SortOrder' value='Ascending'>
+      <label class='radio-label' for='Ascending'>Low to High</label>
+      <input type='radio' id='SortOrder' name='SortOrder' value='Descending'>
+       <label class='radio-label' for='Descending'>High to Low</label>
+       </div>
+          """;
+
+    html = html + "<button class='showTable' type='submit' class='btn btn-primary'>Show Table</button>";
+    
     html = html + "</form>";
+
+    String geoLocation = context.formParam("GeoLocation_drop");
+    String country1 = context.formParam("Country1_drop");
+    String state1 = context.formParam("State1_drop");
+    String city1 = context.formParam("City1_drop");
+    String startYear1 = context.formParam("StartYear1_drop");
+    String timePeriod = context.formParam("TimePeriod_drop");
+    String startYear2 = context.formParam("StartYear2_drop");
+    String startYear3 = context.formParam("StartYear3_drop");
+    String startYear4 = context.formParam("StartYear4_drop");
+    String startYear5 = context.formParam("StartYear5_drop");
+    String country2 = context.formParam("Country2_drop");
+    String country3 = context.formParam("Country3_drop");
+    String country4 = context.formParam("Country4_drop");
+    String sort = context.formParam("SortOrder");
+
+
 
     // Close Content div
     html = html + "</div>";
